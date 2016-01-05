8f4bb08fdf733355c846195f89e73cd62d4b3535
Keron Williams <keronwilliams9@gmail.com>
2016-01-05 11:40:06 -0500

    +Added animations

--- FinalGetWell2/FinalGetWell2/PlaylistTableViewController.swift
+++ FinalGetWell2/FinalGetWell2/PlaylistTableViewController.swift
@@ -28,6 +28,34 @@ class PlaylistTableViewController: UITableViewController
         //         self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
             }
+    override func viewWillAppear(animated: Bool) {
+        animateTable()
+    }
+
+    
+    func animateTable() {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.size.height
+        
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransformMakeTranslation(0, tableHeight)
+        }
+        
+        var index = 0
+        
+        for a in cells {
+            let cell: UITableViewCell = a as UITableViewCell
+            UIView.animateWithDuration(1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .CurveEaseInOut, animations: {
+                cell.transform = CGAffineTransformMakeTranslation(0, 0);
+                }, completion: nil)
+            
+            index += 1
+        }
+    }
+
     
     
     override func didReceiveMemoryWarning()
